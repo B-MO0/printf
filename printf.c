2859ba5d740d58bac09ee07eb260089dda5498ff
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2023-11-13 10:09:32 -0800

    :(

--- printf.c
+++ printf.c
@@ -1,6 +1,7 @@
 #include "main.h"
+#include <string.h>
 
-
+int _putchar(char c);
 int print_number(int n);
 
 /**
@@ -12,18 +13,16 @@ int print_number(int n);
 */
 int _printf(const char *format, ...)
 {
+if (format == NULL)
+return (-1);
 int block_print = 0;
 va_list arguments_list;
-if (format == NULL)
-{
-return (-1);
-}
 va_start(arguments_list, format);
 while (*format)
 {
 if (*format != '%')
 {
-write(1, format, 1);
+_putchar(*format);
 block_print++;
 }
 else
@@ -31,23 +30,15 @@ else
 format++;
 if (*format == '\0')
 break;
-if (*format == '%')
+if (*format == '%' || *format == 'c')
 {
-write(1, format, 1);
-block_print++;
-}
-else if (*format == 'c')
-{
-char c = va_arg(arguments_list, int);
-write(1, &c, 1);
+_putchar(*format);
 block_print++;
 }
 else if (*format == 's')
 {
 char *str = va_arg(arguments_list, char *);
-int str_len = 0;
-while (str[str_len] != '\0')
-str_len++;
+int str_len = strlen(str);
 write(1, str, str_len);
 block_print += str_len;
 }
@@ -60,9 +51,8 @@ block_print += print_number(num);
 format++;
 }
 va_end(arguments_list);
-return block_print;
+return (block_print);
 }
-
 /**
 *print_number - Prints an integer
 *@n: Integer to be printed
@@ -72,18 +62,25 @@ return block_print;
 int print_number(int n)
 {
 int count = 0;
-char digit;
 if (n < 0)
 {
-write(1, "-", 1);
+_putchar('-');
 n = -n;
 count++;
 }
 if (n >= 10)
-{
 count += print_number(n / 10);
+_putchar((n % 10) + '0');
+return (count + 1);
 }
-digit = n % 10 + '0';
-write(1, &digit, 1);
-return count + 1;
+
+/**
+*_putchar - Writes a character to standard output
+*@c: Character to be written
+*
+*Return: On success, returns the character written; On error, returns -1
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
 }
