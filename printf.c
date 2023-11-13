41b74eba0cfc43cf2bd6e594ab6e14dea62aa51f
GithinjiJustus <justusgithinji257@gmail.com>
2023-11-13 08:24:17 -0800

    Refactored code so beautiful, even the code itself blushed. 💅✨ #CodeGlowUp

--- printf.c
+++ printf.c
@@ -2,54 +2,81 @@
 
 int _printf(const char *format, ...)
 {
-int block_print = 0;
-va_list arguments_list;
+	int block_print = 0;
+	va_list arguments_list;
 
-if (format == NULL)
-{
-return (-1);
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(arguments_list, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			block_print++;
+		}
+		else
+		{
+			format++;
+
+			if (*format == '\0')
+				break;
+
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				block_print++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(arguments_list, int);
+				write(1, &c, 1);
+				block_print++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(arguments_list, char *);
+				int str_len = 0;
+				while (str[str_len] != '\0')
+					str_len++;
+				write(1, str, str_len);
+				block_print += str_len;
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(arguments_list, int);
+				block_print += print_number(num);
+			}
+		}
+		format++;
+	}
+
+	va_end(arguments_list);
+	return block_print;
 }
 
-va_start(arguments_list, format);
+int print_number(int n)
+{
+	int count = 0;
+	char digit;
 
-while (*format)
-{
-if (*format != '%')
-{
-write(1, format, 1);
-block_print++;
-}
-else
-{
-format++;
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = -n;
+		count++;
+	}
 
-if (*format == '\0')
-break;
+	if (n / 10)
+	{
+		count += print_number(n / 10);
+	}
 
-if (*format == '%')
-{
-write(1, format, 1);
-block_print++;
-}
-else if (*format == 'c')
-{
-char c = va_arg(arguments_list, int);
-write(1, &c, 1);
-block_print++;
-}
-else if (*format == 's')
-{
-char *str = va_arg(arguments_list, char *);
-int str_len = 0;
-while (str[str_len] != '\0')
-str_len++;
-write(1, str, str_len);
-block_print += str_len;
-}
-}
-format++;
-}
-
-va_end(arguments_list);
-return block_print;
+	digit = n % 10 + '0';
+	write(1, &digit, 1);
+	return count + 1;
 }
