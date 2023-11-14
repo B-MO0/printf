c76490d3d50fd1828ba85694fd9901cf8b935894
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2023-11-14 13:20:04 -0800

    ff

--- old.c
+++ old.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-*_printf - Custom printf function
+*_printf - printf function
 *@format: Format string with placeholders
+*@...: Variable number of arguments
 *
 *Return: Number of characters printed (excluding null byte)
 */
@@ -10,12 +11,11 @@ int _printf(const char *format, ...)
 {
 int block_print = 0;
 va_list args_list;
-
 if (format == NULL)
+{
 return (-1);
-
+}
 va_start(args_list, format);
-
 while (*format)
 {
 if (*format != '%')
@@ -26,10 +26,8 @@ block_print++;
 else
 {
 format++;
-
 if (*format == '\0')
 break;
-
 if (*format == '%')
 {
 write(1, format, 1);
@@ -45,10 +43,8 @@ else if (*format == 's')
 {
 char *str = va_arg(args_list, char *);
 int str_len = 0;
-
 while (str[str_len] != '\0')
 str_len++;
-
 write(1, str, str_len);
 block_print += str_len;
 }
@@ -63,4 +59,29 @@ format++;
 
 va_end(args_list);
 return (block_print);
+}
+
+/**
+*print_number - Prints an integer
+*@n: Integer to be printed
+*
+*Return: Number of characters printed
+*/
+int print_number(int n)
+{
+int count = 0;
+char digit;
+if (n < 0)
+{
+write(1, "-", 1);
+n = -n;
+count++;
+}
+if (n / 10)
+{
+count += print_number(n / 10);
+}
+digit = n % 10 + '0';
+write(1, &digit, 1);
+return (count + 1);
 }
